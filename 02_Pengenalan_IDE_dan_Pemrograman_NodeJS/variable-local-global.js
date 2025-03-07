e2d6a741c30f77305c809b788b2dba379968eb02
daffapasker <162289697+daffapasker@users.noreply.github.com>
2025-03-07 14:00:12 +0700

    Create variable-local-global.js

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Allaya Daffa Zhillal"
+
+function getMyIdentity() {
+    let localVariable = 2211104090
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
