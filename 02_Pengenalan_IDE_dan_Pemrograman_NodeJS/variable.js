9a95c06ea53f06b342f133c3d6163c424931e6f5
daffapasker <162289697+daffapasker@users.noreply.github.com>
2025-03-07 14:01:13 +0700

    Create variable.js

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable.js
@@ -0,0 +1,17 @@
+var name = 'Allaya Daffa Zhillal'
+const nim = 2211104090
+
+console.log(name)
+console.log(nim)
+
+console.log('nama saya adalah ' + name + ' dengan NIM ' + nim)
+console.log(`nama saya adalah ${name} dengan NIM ${nim}`)
+
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Allaya Daffa Zhillal"
