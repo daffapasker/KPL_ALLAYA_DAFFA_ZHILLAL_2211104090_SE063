2518a4c55f49e21af12891060109e74c322099a7
daffapasker <162289697+daffapasker@users.noreply.github.com>
2025-03-29 05:24:53 +0700

    Create implementasi-generic-method.js

---
+++ 05_Generics/jurnal/implementasi-generic-method.js
@@ -0,0 +1,9 @@
+class Penjumlahan {
+    // Method dengan generic
+    JumlahTigaAngka(a, b, c) {
+        let hasil = Number(a) + Number(b) + Number(c);
+        return hasil;
+    }
+}
+
+module.exports = Penjumlahan;  
